d3058c44cc7c0f2d8419a733172f204fd8640d2f
Christopher Osborn <z68ozz@gmail.com>
2020-04-20 01:40:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif array.length > 2 
-    array[-1].insert ()  
+    array[-1].insert (0, "and")
+    
     
   
   
